f5bc590b161a24b9884f1637e844946c0c183d70
rsyu <rustrafa@gmail.com>
2025-03-26 14:23:52 +0300

    Добавлено консольное приложение с использованием picocli и Makefile для удобного запуска

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.", help = true)
+    private boolean helpRequested;
+
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+    private boolean versionRequested;
+
+    @Override
+    public void run() {
+        if (helpRequested) {
+            CommandLine.usage(this, System.out);
+        } else if (versionRequested) {
+            System.out.println("gendiff version 1.0");
+        } else {
+            System.out.println("Hello World!");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CommandLine.run(new App(), args);
     }
 }
