7b961542655a37503492de6554cc6186ce72962f
rsyu <rustrafa@gmail.com>
2025-06-26 12:32:38 +0300

    update

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Rsyu/java-project-71/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Rsyu/java-project-71/actions)
 
+[![Java CI](https://github.com/YOUR_USERNAME/YOUR_REPO/actions/workflows/build.yml/badge.svg)](https://github.com/YOUR_USERNAME/YOUR_REPO/actions/workflows/build.yml)
+
+
 ## Сравнение плоских файлов (JSON)
  https://asciinema.org/a/KJ0Nr6SBVhAPzpS1kOxy6ljgC
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.51.0"
     id("application")
+    id("checkstyle")
+    id("jacoco")
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 group = "hexlet.code"
@@ -15,6 +18,18 @@ repositories {
     mavenCentral()
 }
 
+checkstyle {
+    toolVersion = "10.25.0"
+    configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+}
+
+tasks.withType<Checkstyle> {
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -26,3 +41,29 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+jacoco {
+    toolVersion = "0.8.10"
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Rsyu_java-project-71")
+        property("sonar.organization", "rsyu")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+
